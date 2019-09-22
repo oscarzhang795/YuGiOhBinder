6f191e0605c1c2414cc9fda9b3599581bb5b44da
Oscar <oscarzhang795@gmail.com>
2019-09-22 16:27:23 -0700

    Created card class

---
+++ app/src/main/java/com/oscarz/yu_gi_ohbinder/Card.kt
@@ -0,0 +1,14 @@
+package com.oscarz.yu_gi_ohbinder
+
+import java.util.*
+
+class Card {
+    lateinit var productId: String
+    lateinit var name: String
+    lateinit var cleanName: String
+    lateinit var imageUrl: String
+    lateinit var categoryId: String
+    lateinit var groupId: String
+    lateinit var url: String
+    lateinit var modifiedon: Date
+}
