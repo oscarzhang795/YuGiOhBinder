05a992fd18f4a68ec084e0930b923bffd9fbccdc
oscarzhang795 <oscarzhang795@gmail.com>
2019-12-11 23:03:42 -0800

    Figured out how to get auth token from server

--- app/src/main/java/com/oscarz/yu_gi_ohbinder/TCGAPIConnector.kt
+++ app/src/main/java/com/oscarz/yu_gi_ohbinder/TCGAPIConnector.kt
@@ -1,32 +1,31 @@
 package com.oscarz.yu_gi_ohbinder
 
-import android.os.Build
-import android.util.Log
-import com.android.volley.Request
 import com.android.volley.Response
-import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import org.json.JSONObject
 
 class TCGAPIConnector: WebService {
 
-    val url = "https://api.tcgplayer.com/token/grant_type=client_credentials&client_id=PUBLIC_KEY&client_secret=PRIVATE_KEY"
+    val url = "https://api.tcgplayer.com/token"
     val queue = Volley.newRequestQueue(BinderApplication.appContext)
     fun getAuthToken() {
-        val jsonBody = JSONObject()
-        jsonBody.put("grant_type", "client_credentials")
-        jsonBody.put("client_id", BuildConfig.PUBLIC_ID)
-        jsonBody.put("client_secret", BuildConfig.PRIVATE_ID)
-
-        val request = StringRequest(Request.Method.POST, url,
+        val request = object: StringRequest(
+            Method.POST, url,
             Response.Listener {
 
             },
             Response.ErrorListener {
 
             }
-        )
+        ) {
+            override fun getParams(): MutableMap<String, String> {
+                val map = HashMap<String, String>()
+                map["grant_type"] = "client_credentials"
+                map["client_id"] = BuildConfig.PUBLIC_ID
+                map["client_secret"] = BuildConfig.PRIVATE_ID
+                return map
+            }
+        }
         queue.add(request)
     }
 }
